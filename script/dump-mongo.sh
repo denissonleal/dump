cd22473d7335b3b86dcbfbcc8a13e1a28707211b
leal <denissonleal@gmail.com>
2016-07-17 19:15:09 -0300

    correção de bug ao deletar arquivo

--- script/dump-mongo.sh
+++ script/dump-mongo.sh
@@ -9,7 +9,7 @@ mongodump
 7z a $FILE_DUMP dump/
 rm -r dump/
 curl -F "file=@./$FILE_DUMP" $URL
-rm FILE_DUMP
+rm $FILE_DUMP
 
 date +%Y-%m-%d\ %H:%M:%S
 # curl -F "file=@./dump-esus-cidadesaudavel-2016-07-17-12-26-12.7z" http://dump.sysvale.com/dump
