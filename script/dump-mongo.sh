02849a258844297fcfbcbae8b27ad2765722bd84
leal <denissonleal@gmail.com>
2016-07-17 13:23:19 -0300

    mudança no host do script

--- script/dump-mongo.sh
+++ script/dump-mongo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
+FILE_DUMP=dump-$(hostname)-$(date +%Y-%m-%d-%H-%M-%S).7z
 rm -r dump/ dump-$(hostname)-*.7z
 mongodump
-FILE_DUMP=dump-$(hostname)-$(date +%Y-%m-%d-%H-%M-%S).7z
 7z a $FILE_DUMP dump/
-curl -F "file=@./$FILE_DUMP" http://localhost:8000/dump
+curl -F "file=@./$FILE_DUMP" http://dump.sysvale.com/dump
