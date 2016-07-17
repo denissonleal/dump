11c1cef16454715ffb1b7c7ed15ff2052f039ede
leal <denissonleal@gmail.com>
2016-07-17 18:17:57 -0300

    melhorias no script mongo dump

--- script/dump-mongo.sh
+++ script/dump-mongo.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+date +%Y-%m-%d\ %H:%M:%S
+
 FILE_DUMP=dump-$(hostname)-$(date +%Y-%m-%d-%H-%M-%S).7z
-rm -r dump/ dump-$(hostname)-*.7z
+URL=http://dump.sysvale.com/dump?hostname=$(hostname)
+
 mongodump
 7z a $FILE_DUMP dump/
-curl -F "file=@./$FILE_DUMP" http://dump.sysvale.com/dump
+rm -r dump/
+curl -F "file=@./$FILE_DUMP" $URL
+rm FILE_DUMP
+
+date +%Y-%m-%d\ %H:%M:%S
+# curl -F "file=@./dump-esus-cidadesaudavel-2016-07-17-12-26-12.7z" http://dump.sysvale.com/dump
