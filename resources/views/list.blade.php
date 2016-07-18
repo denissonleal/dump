23c30b8ad9c11f0e30f97f4ca5106b77ba55f1ab
leal <denissonleal@gmail.com>
2016-07-18 00:23:50 -0300

    lista centralizada

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -6,15 +6,17 @@
 		<title>Dump</title>
 	</head>
 	<body>
-		<h3>List of last dumps</h3>
-		<ul>
-			@foreach($hostnames as $hostname)
+		<div style="text-align: center;">
+			<h3>List of last dumps</h3>
+			<ul style="text-align: left; width: 500px; margin: auto; max-width: 90%;">
+				@foreach($hostnames as $hostname)
 				<li>
 					<b>{{ $hostname->name }}</b>
 					<i>{{ number_format($hostname->dump->size/1000000., 3, '.', ' ') }}</i> MB
 					<small>{{ $hostname->dump->created_at->format('Y-m-d H:i:s') }}</small>
 				</li>
-			@endforeach
-		</ul>
+				@endforeach
+			</ul>
+		</div>
 	</body>
 </html>
