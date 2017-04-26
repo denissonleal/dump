7bf86622ab74ce74252d10825f9d9a93bf336276
Denisson Leal <denissonleal@juazeiro.esus.com>
2017-04-26 10:33:35 -0300

    table format and free space

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,13 +6,22 @@ use App\Hostname;
 
 class HomeController extends Controller
 {
-  public function anyIndex()
-  {
-    $hostnames = Hostname::orderBy('name')->get();
-    foreach ($hostnames as $hostname) {
-      $hostname->dump = $hostname->dumps()->orderBy('created_at', 'desc')->first();
-    }
-    // dd($hostnames->toArray());
-    return view('list', ['hostnames' => $hostnames]);
-  }
+	public function anyIndex()
+	{
+		$hostnames = Hostname::orderBy('name')->get();
+		foreach ($hostnames as $hostname) {
+			$hostname->dump = $hostname->dumps()->orderBy('created_at', 'desc')->first();
+		}
+		$bytes = disk_free_space(storage_path('dumps'));
+		$si_prefix = ['B', 'KB', 'MB', 'GB', 'TB', 'EB', 'ZB', 'YB'];
+		$base = 1024;
+		$class = min((int)log($bytes , $base) , count($si_prefix) - 1);
+		// echo $bytes . '<br />';
+		// echo sprintf('%1.2f' , $bytes / pow($base,$class)) . ' ' . $si_prefix[$class] . '<br />';
+		// dd($hostnames->toArray());
+		return view('list', [
+			'hostnames' => $hostnames,
+			'free' => sprintf('%1.2f %s' , $bytes / pow($base,$class), $si_prefix[$class]),
+		]);
+	}
 }
--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -3,12 +3,37 @@
 	<head>
 		<meta name = "viewport" content = "user-scalable=no, initial-scale=1.0, maximum-scale=1.0, width=device-width">
 		<meta charset="utf-8">
+		<meta http-equiv="refresh" content="1800">
 		<title>Dump</title>
 	</head>
 	<body>
 		<div style="text-align: center;">
 			<h3>List of last dumps</h3>
-			<ul style="text-align: left; width: 500px; margin: auto; max-width: 90%;">
+			<table style="text-align: left; width: 500px; margin: auto; max-width: 90%;">
+				<thead>
+					<tr>
+						<th>#</th>
+						<th>Server</th>
+						<th>Size</th>
+						<th>Date</th>
+					</tr>
+				</thead>
+				<tbody>
+					@foreach($hostnames as $i => $hostname)
+					<tr>
+						<td>{{ $i+1 }}</td>
+						<td><b>{{ $hostname->name }}</b></td>
+						<td align="right"><i>{{ number_format($hostname->dump->size/1000000., 3, '.', ' ') }}</i> MB</td>
+						<td><small>{{ $hostname->dump->created_at->format('Y-m-d H:i:s') }}</small></td>
+					</tr>
+					@endforeach
+				</tbody>
+			</table>
+			<p>
+				Free space {{ $free }}
+			</p>
+
+			{{-- <ul style="text-align: left; width: 500px; margin: auto; max-width: 90%;">
 				@foreach($hostnames as $hostname)
 				<li>
 					<b>{{ $hostname->name }}</b>
@@ -16,7 +41,7 @@
 					<small>{{ $hostname->dump->created_at->format('Y-m-d H:i:s') }}</small>
 				</li>
 				@endforeach
-			</ul>
+			</ul> --}}
 		</div>
 	</body>
 </html>
